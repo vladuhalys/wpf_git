137e840d458b968b0c832427353481fa56ae8c01
Олег Замрій <mynameishelg@gmail.com>
2024-06-16 12:50:40 +0300

    added HR class

---
+++ HR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_git
+{
+    namespace HRApp
+    {
+        public class HR
+        {
+            public string Name { get; set; }
+            public string Position { get; set; }
+            public string Department { get; set; }
+        }
+    }
+
+}
