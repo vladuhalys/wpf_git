91fa2c18a297b66092cd1669c271d281aa482b80
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-16 12:47:13 +0300

    Added user info

--- Models/User.cs
+++ Models/User.cs
@@ -11,16 +11,19 @@ namespace wpf_git.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        private string PhoneNumber { get; set; }
-        private string location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Location { get; set; }
 
-        public User(string name, string email, string password, string phoneNumber, string location)
+        public User(string name, string email, string password)
         {
             Name = name;
             Email = email;
             Password = password;
-            PhoneNumber = phoneNumber;
-            this.location = location;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Email: {Email}, PhoneNumber: {PhoneNumber}, Location: {Location}";
         }
     }
 }
